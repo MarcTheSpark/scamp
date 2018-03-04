390566d8e0c96df4b2a862f7177db4e144607bd7
MarcTheSpark <marc.p.evans@gmail.com>
2018-03-04 15:26:06 -0800

    Working combined MIDI player that both talks to fluidsynth and outputs a midi stream via rt_midi.

--- source/combined_midi_player.py
+++ source/combined_midi_player.py
@@ -1,25 +1,22 @@
 from .playcorder_utilities import resolve_relative_path
+from .simple_rtmidi_wrapper import SimpleRtMidiOut
+from collections import OrderedDict
 
 fluidsynth_loaded = False
 try:
     from .thirdparty.fluidsynth import Synth
     fluidsynth_loaded = True
 except ImportError:
-    "Fluidsynth could not be loaded; synth output will not be available."
-
-rtmidi_loaded = False
-try:
-    import rtmidi
-    rtmidi_loaded = True
-except ImportError:
-    "python-rtmidi was not found; midi output will not be available."
+    Synth = None
+    print("Fluidsynth could not be loaded; synth output will not be available.")
 
 sf2utils_loaded = False
 try:
     from sf2utils.sf2parse import Sf2File
     sf2utils_loaded = True
 except ImportError:
-    "sf2utils was not found; info about soundfont presets will not be available."
+    Sf2File = None
+    print("sf2utils was not found; info about soundfont presets will not be available.")
 
 
 # load up all of the default soundfonts and their paths
@@ -57,30 +54,9 @@ def unregister_default_soundfont(name):
         defaults_file.writelines([', '.join(x) + '\n' for x in _defaultSoundfonts.items()])
 
 
-def get_best_name_match(names_list, desired_name):
-    """
-    Looks initially for first exact case-sensitive match for desired_name in names_list. Then looks for
-    first case insensitive match. Then looks for anything containing desired_name (case insensitive).
-    Outputs None if this all fails
-    :param names_list: list of names to search through
-    :param desired_name: name to match
-    :return: str of best match to desired name in names_list
-    """
-    if desired_name in names_list:
-        return names_list.index(desired_name)
-    lower_case_list = [s.lower() for s in names_list]
-    lower_case_desired_name = desired_name.lower()
-    if lower_case_desired_name in lower_case_list:
-        return lower_case_list.index(lower_case_desired_name)
-    for lower_case_name in lower_case_list:
-        if lower_case_desired_name in lower_case_name:
-            return lower_case_list.index(lower_case_name)
-    return None
-
-
 class CombinedMidiPlayer:
 
-    def __init__(self, soundfonts=None):
+    def __init__(self, soundfonts=None, driver=None, rtmidi_output_device=None):
 
         self.used_channels = 0  # how many channels have we already assigned to various instruments
 
@@ -109,6 +85,15 @@ class CombinedMidiPlayer:
 
             self.initialize_fluidsynth(soundfont_paths, driver=driver)
 
+        self.default_rtmidi_output_device = rtmidi_output_device
+
+    def add_instrument(self, num_channels, bank_and_preset, soundfont=0,
+                       midi_output_device=None, midi_output_name=None):
+        midi_output_device = self.default_rtmidi_output_device if self.default_rtmidi_output_device is not None \
+            else midi_output_device
+        return CombinedMidiInstrument(self, num_channels, bank_and_preset, soundfont_id=self.soundfont_ids[soundfont],
+                                      midi_output_device=midi_output_device, midi_output_name=midi_output_name)
+
     def initialize_fluidsynth(self, soundfont_paths, driver=None):
         # loads the soundfonts and gets the synth going
         self.synth = Synth()
@@ -126,26 +111,20 @@ class CombinedMidiPlayer:
 
 class CombinedMidiInstrument:
 
-    def __init__(self, combined_midi_player, num_channels, bank_and_preset=None, soundfont_id=0,
+    def __init__(self, combined_midi_player, num_channels, bank_and_preset, soundfont_id=0,
                  midi_output_device=None, midi_output_name=None):
         assert isinstance(combined_midi_player, CombinedMidiPlayer)
         self.combined_midi_player = combined_midi_player
         self.channels = list(range(self.combined_midi_player.used_channels,
                                    self.combined_midi_player.used_channels + num_channels))
+        self.num_channels = num_channels
+        self.combined_midi_player.used_channels += num_channels
         self.bank_and_preset = bank_and_preset
         self.soundfont_id = soundfont_id
 
-        if rtmidi_loaded:
-            self.midiout = rtmidi.MidiOut()
-            if isinstance(midi_output_device, int):
-                self.midiout.open_port(midi_output_device, name=midi_output_name)
-            else:
-                available_ports = self.midiout.get_ports()
-                chosen_output = get_best_name_match(available_ports, midi_output_device)
-                if chosen_output is not None:
-                    self.midiout.open_port(chosen_output, name=midi_output_name)
-                else:
-                    self.midiout.open_virtual_port(name=midi_output_name)
+        self.max_pitch_bend = 2
+
+        self.rt_simple_out = SimpleRtMidiOut(midi_output_device, midi_output_name)
 
         if fluidsynth_loaded:
             self.set_to_preset(*bank_and_preset)
@@ -155,19 +134,60 @@ class CombinedMidiInstrument:
             self.combined_midi_player.synth.program_select(i, self.soundfont_id, bank, preset)
 
     def note_on(self, chan, pitch, volume_from_0_to_1):
+        """
+        NB: for this and following commands, since a single instance of fluidsynth is running for all instruments,
+        it we need to use the absolute channel when we call it 9I.. Each rt_midi output, on the other hand is local
+        to the instrument, so no such conversion is necessary.
+        """
         absolute_channel = self.channels[chan]
-        self.combined_midi_player.synth.noteon(absolute_channel, pitch, int(volume_from_0_to_1 * 127))
+        velocity = int(volume_from_0_to_1 * 127)
+        self.combined_midi_player.synth.noteon(absolute_channel, pitch, velocity)
+        self.rt_simple_out.note_on(chan, pitch, velocity)
 
     def note_off(self, chan, pitch):
         absolute_channel = self.channels[chan]
         self.combined_midi_player.synth.noteon(absolute_channel, pitch, 0)  # note on call of 0 velocity implementation
         self.combined_midi_player.synth.noteoff(absolute_channel, pitch)  # note off call implementation
+        self.rt_simple_out.note_off(chan, pitch)
 
-    def set_pitch_bend(self, chan, bend_in_semitones):
+    def pitch_bend(self, chan, bend_in_semitones):
         absolute_channel = self.channels[chan]
-        pitch_bend_val = int(bend_in_semitones*4096)
-        self.combined_midi_player.synth.pitch_bend(absolute_channel, pitch_bend_val)
+        directional_bend_value = int(bend_in_semitones / self.max_pitch_bend * 8192)
+        # we can't have a directional pitch bend popping up to 8192, because we'll go one above the max allowed
+        # on th other hand, -8192 is fine, since that will add up to zero
+        directional_bend_value = max(-8192, min(directional_bend_value, 8191))
+        # for some reason, pyFluidSynth takes a value from -8192 to 8191 and then adds 8192 to it
+        self.combined_midi_player.synth.pitch_bend(absolute_channel, directional_bend_value)
+        # rt_midi wants the normal value having added in 8192
+        self.rt_simple_out.pitch_bend(chan, directional_bend_value + 8192)
 
-    def set_expression(self, chan, expression_from_0_to_1):
+    def set_max_pitch_bend(self, max_bend_in_semitones):
+        """
+        Sets the maximum pitch bend to the given number of semitones up and down for all tracks associated
+        with this instrument. Note that, while this will definitely work with fluidsynth, the output of rt_midi
+        must be being recorded already for this to affect subsequent pitch bend, which is slightly awkward.
+        Also, in my experience, even then it may be ignored.
+        :type max_bend_in_semitones: int
+        :return: None
+        """
+        if not isinstance(max_bend_in_semitones, int):
+            print("Max pitch bend must be an integer number of semitones.")
+            return
+        for chan in range(self.num_channels):
+            absolute_channel = self.channels[chan]
+            self.combined_midi_player.synth.cc(absolute_channel, 101, 0)
+            self.rt_simple_out.cc(chan, 101, 0)
+            self.combined_midi_player.synth.cc(absolute_channel, 100, 0)
+            self.rt_simple_out.cc(chan, 100, 0)
+            self.combined_midi_player.synth.cc(absolute_channel, 6, max_bend_in_semitones)
+            self.rt_simple_out.cc(chan, 6, max_bend_in_semitones)
+            self.combined_midi_player.synth.cc(absolute_channel, 100, 127)
+            self.rt_simple_out.cc(chan, 100, 127)
+            self.max_pitch_bend = max_bend_in_semitones
+
+    def expression(self, chan, expression_from_0_to_1):
         absolute_channel = self.channels[chan]
-        self.combined_midi_player.synth.cc(absolute_channel, 11, int(expression_from_0_to_1 * 127))
+        expression_val = int(expression_from_0_to_1 * 127)
+        self.combined_midi_player.synth.cc(absolute_channel, 11, expression_val)
+        self.rt_simple_out.expression(chan, expression_val)
+
---
+++ source/simple_rtmidi_wrapper.py
@@ -0,0 +1,78 @@
+rtmidi_loaded = False
+try:
+    import rtmidi
+    rtmidi_loaded = True
+except ImportError:
+    rtmidi = None
+    print("python-rtmidi was not found; midi output will not be available.")
+
+
+def get_best_name_match(names_list, desired_name):
+    """
+    Looks initially for first exact case-sensitive match for desired_name in names_list. Then looks for
+    first case insensitive match. Then looks for anything containing desired_name (case insensitive).
+    Outputs None if this all fails
+    :param names_list: list of names to search through
+    :param desired_name: name to match
+    :return: str of best match to desired name in names_list
+    """
+    if desired_name is None:
+        return None
+    if desired_name in names_list:
+        return names_list.index(desired_name)
+    lower_case_list = [s.lower() for s in names_list]
+    lower_case_desired_name = desired_name.lower()
+    if lower_case_desired_name in lower_case_list:
+        return lower_case_list.index(lower_case_desired_name)
+    for lower_case_name in lower_case_list:
+        if lower_case_desired_name in lower_case_name:
+            return lower_case_list.index(lower_case_name)
+    return None
+
+
+class SimpleRtMidiOut:
+    """
+    Wraps a single output of rtmidi to:
+    a) make the calls a little easier and more specific, rather than all being send_message
+    b) fail quietly. If rtmidi can't be loaded, then the user is alerted upon import, and
+    from then on all rtmidi calls just don't do anything
+    """
+    def __init__(self, output_device=None, output_name=None):
+
+        if rtmidi_loaded:
+            self.midiout = rtmidi.MidiOut()
+            if isinstance(output_device, int):
+                self.midiout.open_port(output_device, name=output_name)
+            else:
+                available_ports = self.midiout.get_ports()
+                chosen_output = get_best_name_match(available_ports, output_device)
+                if chosen_output is not None:
+                    self.midiout.open_port(chosen_output, name=output_name)
+                else:
+                    self.midiout.open_virtual_port(name=output_name)
+
+    def note_on(self, chan, pitch, velocity):
+        if rtmidi_loaded:
+            self.midiout.send_message([0x90 + chan, pitch, velocity])
+
+    def note_off(self, chan, pitch):
+        if rtmidi_loaded:
+            self.midiout.send_message([0x80 + chan, pitch, 0])  # note on call of 0 velocity implementation
+            self.midiout.send_message([0x90 + chan, pitch, 0])  # note off call implementation
+
+    def pitch_bend(self, chan, value):
+        assert 0 <= value < 16384
+        if rtmidi_loaded:
+            # midi pitch bend data takes two midi data bytes; a least significant 7-bit number and
+            # a most significant 7-bit number. These combine to form an integer from 0 to 16383
+            lsb = value % 128
+            msb = (value - lsb) / 128
+            self.midiout.send_message([0xE0 + chan, lsb, msb])  # note on call of 0 velocity implementation
+
+    def expression(self, chan, value):
+        if rtmidi_loaded:
+            self.midiout.send_message([0xB0 + chan, 11, value])
+
+    def cc(self, chan, cc_number, value):
+        if rtmidi_loaded:
+            self.midiout.send_message([0xB0 + chan, cc_number, value])
