effa366768fa5b965d9d9c99b4ef2b3f218921cb
MarcTheSpark <marc.p.evans@gmail.com>
2018-03-09 23:41:41 -0800

    updated imports of register and unregister sound font functions

--- source/__init__.py
+++ source/__init__.py
@@ -1,3 +1,4 @@
-from .playcorder import Playcorder, register_default_soundfont, unregister_default_soundfont
+from .playcorder import Playcorder, ParameterCurve
+from .combined_midi_player import register_default_soundfont, unregister_default_soundfont
 
 __author__ = 'mpevans'
