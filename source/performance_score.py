297d46924aa3727c0fe24ef414ab00c51f67c045
MarcTheSpark <marc.p.evans@gmail.com>
2018-03-22 00:05:30 -0700

    Started PerformanceScore class

---
+++ source/performance_score.py
@@ -0,0 +1,42 @@
+from sortedcontainers import SortedListWithKey
+from .measures_beats_notes import PCNote
+
+
+class PerformanceScore:
+
+    def __init__(self):
+        self._parts = []
+        self._parts_to_instruments = {}
+
+    def new_part(self, name: str, instrument=None):
+        self._parts.append(PerformancePart(name))
+        if instrument is not None:
+            self.set_part_instrument(instrument, instrument)
+            
+    def append_part(self, part: PerformancePart, instrument=None):
+        self._parts.append(part)
+        if instrument is not None:
+            self.set_part_instrument(instrument, instrument)
+
+    def set_part_instrument(self, part, instrument):
+        self._parts_to_instruments[part] = instrument
+
+    def parts(self):
+        return iter(self._parts)
+    
+    def part(self, index: int):
+        return self._parts[index]
+    
+    def instrument_of_part(self, part):
+        return self._parts_to_instruments[part]
+
+
+class PerformancePart:
+
+    def __init__(self, name):
+        # contains reference to  instruments
+        self.notes = SortedListWithKey(key=lambda note: note.start_time)
+        self.name = name
+
+    def add_note(self, note: PCNote):
+        self.notes.add(note)
