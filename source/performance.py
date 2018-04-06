ecd6cea9c025ded3aeab296e01bcb178f90a604b
MarcTheSpark <marc.p.evans@gmail.com>
2018-04-06 12:31:36 -0700

    Performance and PerformancePart playable on clocks using ensembles

--- source/performance.py
+++ source/performance.py
@@ -1,6 +1,8 @@
 from sortedcontainers import SortedList
 from collections import namedtuple
 from .parameter_curve import ParameterCurve
+from .clock import Clock
+import logging
 
 PerformanceNote = namedtuple("PerformanceNote", "start_time length pitch volume properties")
 
@@ -35,6 +37,66 @@ class PerformancePart:
         self.instrument = instrument
         self._instrument_id = instrument.name, instrument.name_count
 
+    @property
+    def end_time(self):
+        return max(n.start_time + n.length for n in self.notes)
+
+    def get_note_iterator(self, start_time, stop_time):
+        def iterator():
+            note_index = self.notes.bisect_left((start_time,))
+            while note_index < len(self.notes) and self.notes[note_index].start_time < stop_time:
+                yield self.notes[note_index]
+                note_index += 1
+
+        return iterator()
+
+    def play(self, start_time=0, stop_time=None, instrument=None, clock=None, blocking=False):
+        instrument = self.instrument if instrument is None else instrument
+        from playcorder.instruments import PlaycorderInstrument
+        if not isinstance(instrument, PlaycorderInstrument):
+            raise ValueError("PerformancePart does not have a valid instrument and cannot play.")
+        clock = Clock(instrument.name + " clock", pool_size=20) if clock is None else clock
+        if not isinstance(clock, Clock):
+            raise ValueError("PerformancePart was given an invalid clock.")
+        stop_time = self.end_time if stop_time is None else stop_time
+        assert stop_time >= start_time
+
+        def _play_thread(child_clock):
+            note_iterator = self.get_note_iterator(start_time, stop_time)
+            self.get_note_iterator(start_time, stop_time)
+            try:
+                current_note = next(note_iterator)
+            except StopIteration:
+                return
+
+            child_clock.wait(current_note.start_time - start_time)
+
+            while True:
+                instrument.play_note(current_note.pitch, current_note.volume,
+                                     current_note.length, current_note.properties, child_clock)
+                try:
+                    next_note = next(note_iterator)
+                    child_clock.wait(next_note.start_time - current_note.start_time)
+
+                    current_note = next_note
+                except StopIteration:
+                    # when done, wait for the children to finish
+                    child_clock.wait(current_note.length)
+                    return
+
+        if blocking:
+            # clock blocked ;-)
+            _play_thread(clock)
+            return clock
+        else:
+            return clock.fork(_play_thread)
+
+    def set_instrument_from_ensemble(self, ensemble):
+        self.instrument = ensemble.get_instrument_by_name(*self._instrument_id)
+        if self.instrument is None:
+            logging.warning("No matching instrument could be found for part {}.".format(self.name))
+        return self
+
     def __repr__(self):
         return "PerformancePart(name=\"{}\", instrument_id={}, notes=[\n{}\n])".format(
             self.name, self._instrument_id, "   " + ", \n   ".join(str(x) for x in self.notes)
@@ -52,11 +114,9 @@ class PerformancePart:
         ]}
 
     @classmethod
-    def from_json(cls, json_dict, ensemble=None):
+    def from_json(cls, json_dict):
         performance_part = cls(name=json_dict["name"])
         performance_part._instrument_id = json_dict["instrument_id"]
-        if ensemble is not None:
-            performance_part.instrument = ensemble.get_instrument_by_name(*performance_part._instrument_id)
         for note in json_dict["notes"]:
             if hasattr(note["pitch"], "__len__"):
                 note["pitch"] = ParameterCurve.from_json(note["pitch"])
@@ -80,12 +140,47 @@ class Performance:
     def add_part(self, part: PerformancePart):
         self.parts.append(part)
 
+    def get_part_by_index(self, index):
+        return self.parts[index]
+
+    def get_parts_by_name(self, name):
+        return [x for x in self.parts if x.name == name]
+
+    def play(self, start_time=0, stop_time=None, ensemble=None, clock=None, blocking=False):
+        if ensemble is not None:
+            self.set_instruments_from_ensemble(ensemble)
+
+        # if not given a valid MasterClock, create one
+        if not isinstance(clock, Clock):
+            clock = Clock()
+
+        if stop_time is None:
+            stop_time = max(p.end_time for p in self.parts)
+
+        for p in self.parts:
+            p.play(start_time, stop_time, clock=clock, blocking=False)
+
+        if blocking:
+            clock.wait_for_children_to_finish()
+
+        return clock
+
+    def set_instruments_from_ensemble(self, ensemble):
+        for part in self.parts:
+            part.set_instrument_from_ensemble(ensemble)
+        return self
+
     def to_json(self):
         return {"parts": [part.to_json() for part in self.parts]}
 
     @classmethod
-    def from_json(cls, json_dict, ensemble=None):
-        return cls([PerformancePart.from_json(part_json, ensemble) for part_json in json_dict["parts"]])
+    def from_json(cls, json_dict):
+        return cls([PerformancePart.from_json(part_json) for part_json in json_dict["parts"]])
+
+    def save_to_json(self, file_path):
+        import json
+        with open(file_path, "w") as file:
+            json.dump(self.to_json(), file)
 
     def __repr__(self):
         return "Performance([\n{}\n])".format("   " + ",\n   ".join(
