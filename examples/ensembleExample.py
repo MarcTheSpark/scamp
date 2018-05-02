bdb96b53e5aea0d87edcbd1f1331ac8be2cdd74a
MarcTheSpark <marc.p.evans@gmail.com>
2018-05-02 10:18:20 -0700

    working, simple ensemble example

---
+++ examples/ensembleExample.py
@@ -0,0 +1,29 @@
+from playcorder import Ensemble
+
+
+def construct_ensemble():
+    global piano, flute, strings, ensemble
+    ensemble = Ensemble("default", "pulseaudio")
+
+    ensemble.print_all_soundfont_presets()
+
+    piano = ensemble.add_midi_part("piano", (0, 0))
+    flute = ensemble.add_midi_part("flute", (0, 73))
+    strings = ensemble.add_midi_part("strings", (0, 40))
+
+
+def play_some_stuff():
+    while True:
+        piano.play_note(65, 0.5, 1.0)
+        flute.play_note(70, 0.5, 0.25)
+        strings.play_note([75, 73], 0.5, 1.0, blocking=True)
+
+
+# construct_ensemble()
+
+# ensemble.save_to_json("savedEnsemble.json")
+
+# ensemble = Ensemble.load_from_json("savedEnsemble.json")
+# piano, flute, strings = ensemble.instruments
+
+play_some_stuff()
