355179be3ace9250ec7347b3b4a499c946e7e587
marc <marc.p.evans@gmail.com>
2018-09-24 23:02:08 -0700

    Made timing more reasonable

--- examples/glissando_test.py
+++ examples/glissando_test.py
@@ -14,12 +14,13 @@ pc.start_recording()
 while pc.time() < 12:
     gliss = Envelope.from_levels_and_durations(
         [random.random() * 20 + 60, random.random() * 20 + 60, random.random() * 20 + 60, random.random() * 20 + 60],
-        [random.random(), random.random(), random.random()]
+        [random.random()+0.5, random.random()+0.5, random.random()+0.5]
     )
+
     if random.random() < 0.5:
-        piano.play_note(gliss, 1.0, random.random()*2 + 0.2)
+        piano.play_note(gliss, 1.0, random.random()*2 + 0.5)
     else:
-        piano.play_chord([gliss, gliss+4], 1.0, random.random()*2 + 0.2)
+        piano.play_chord([gliss, gliss+4], 1.0, random.random()*2 + 0.5)
     if random.random() < 0.5:
         pc.wait(random.random()*2)
 
