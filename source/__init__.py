864c3217869acea08e6f67f69643db09921a7c16
marc <marc.p.evans@gmail.com>
2018-09-30 10:16:36 -0700

    Added more classes to the playcorder namespace

--- source/__init__.py
+++ source/__init__.py
@@ -1,8 +1,10 @@
 from .playcorder import Playcorder
 from .envelope import Envelope
 from .ensemble import Ensemble
-from .instruments import MidiPlaycorderInstrument, PlaycorderInstrument
+from .instruments import MidiPlaycorderInstrument, PlaycorderInstrument, OSCPlaycorderInstrument
 from .clock import Clock, TempoEnvelope, wait
 from .performance import Performance, PerformancePart
+from .score import *
+from .performance_note import PerformanceNote, NotePropertiesDictionary
 
 __author__ = 'mpevans'
