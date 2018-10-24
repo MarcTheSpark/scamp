23ff7abe8dd97091d8625ac636a735be7dee57f7
marc <marc.p.evans@gmail.com>
2018-10-23 23:49:33 -0700

    finished and added memoization

--- source/spelling.py
+++ source/spelling.py
@@ -1,8 +1,11 @@
+import functools
+
 _c_spellings = ((0, 0), (0, 1), (1, 0), (2, -1), (2, 0), (3, 0), (3, 1), (4, 0), (5, -1), (5, 0), (6, -1), (6, 0))
 _sharp_spellings = ((0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (3, 0), (3, 1), (4, 0), (4, 1), (5, 0), (5, 1), (6, 0))
 _flat_spellings = ((0, 0), (1, -1), (1, 0), (2, -1), (2, 0), (3, 0), (4, -1), (4, 0), (5, -1), (5, 0), (6, -1), (6, 0))
 _step_names = ('c', 'd', 'e', 'f', 'g', 'a', 'b')
-_sharp_order = (3, 0, 4, 1, 5, 2, 6)
+_step_circle_of_fifths_positions = (0, 2, 4, -1, 1, 3, 5)  # number of sharps or flats for the white keys
+_sharp_order = (3, 0, 4, 1, 5, 2, 6)  # order in which sharps are added to a key signature
 _flat_order = tuple(reversed(_sharp_order))
 
 
@@ -10,20 +13,26 @@ class SpellingPolicy:
 
     def __init__(self, step_alteration_pairs=_c_spellings):
         """
-        Object that translates pitches or pitch classes the actual spelling used in a score
+        Object that translates pitches or pitch classes to the actual spelling used in a score
+        Note that functools.lru_cache results in the same classmethod calls returning identical objects
+        This is valuable because if we play a bunch of notes with properties="spelling: D major", then each time
+        that string is converted to a SpellingPolicy class, it gets to reuse the same instance.
         :param step_alteration_pairs: a container of 12 (step, alteration) tuples shoeing how to spell each pitch class
         """
         self.step_alteration_pairs = step_alteration_pairs
 
     @classmethod
+    @functools.lru_cache()
     def all_sharps(cls):
         return cls(_sharp_spellings)
 
     @classmethod
+    @functools.lru_cache()
     def all_flats(cls):
         return cls(_flat_spellings)
 
     @classmethod
+    @functools.lru_cache()
     def from_circle_of_fifths_position(cls, num_sharps_or_flats, avoid_double_accidentals=False):
         if num_sharps_or_flats == 0:
             return c_spellings
@@ -47,6 +56,34 @@ class SpellingPolicy:
             new_spellings.append((new_step, new_alteration))
 
         return cls(tuple(sorted(new_spellings, key=lambda x: (_c_spellings.index((x[0], 0)) + x[1]) % 12)))
+
+    @classmethod
+    @functools.lru_cache()
+    def from_string(cls, string_initializer: str):
+        """
+        Creates an instance of SpellingPolicy from several possible input string formats
+        :param string_initializer: one of the following:
+            - "flat" or "sharp", indicating that black keys are always expressed a particular way
+            - a key center (case insensitive), such as "C#" or "f" or "Gb"
+            - key centers with modes attached (such as "g minor") are fine, too, but the mode is ignored since it
+            doesn't actually affect the way that the accidentals are interpreted.
+        """
+        if string_initializer == "flat":
+            return SpellingPolicy.all_flats()
+        elif string_initializer == "sharp":
+            return SpellingPolicy.all_sharps()
+        else:
+            string_initializer_lower = string_initializer.lower().replace(" ", "")
+            try:
+                num_sharps_or_flats = _step_circle_of_fifths_positions[_step_names.index(string_initializer_lower[0])]
+            except ValueError:
+                raise ValueError("Bad spelling policy initialization string. Use only 'sharp', 'flat', "
+                                 "or the name of the desired key center (e.g. 'G#' or 'Db')")
+            if string_initializer_lower[1:].startswith(("b", "flat", "f", "-f", "-flat")):
+                num_sharps_or_flats -= 7
+            elif string_initializer_lower[1:].startswith(("#", "sharp", "s", "-s", "-sharp")):
+                num_sharps_or_flats += 7
+            return SpellingPolicy.from_circle_of_fifths_position(num_sharps_or_flats)
 
     def get_name_alteration_and_octave(self, midi_num):
         rounded_midi_num = int(round(midi_num))
