e526483a8438fcbb6b1f9b0361053be845bf4f09
MarcTheSpark <marc.p.evans@gmail.com>
2019-06-26 11:27:22 -0700

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scamp",
-    version="0.2.1",
+    version="0.3",
     author="Marc Evanstein",
     author_email="marc@marcevanstein.com",
     description="An algorithmic composition framework that manages the flow of musical time, plays back notes via "
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/MarcTheSpark/scamp",
     packages=setuptools.find_packages(),
-    install_requires=['pymusicxml >= 0.1.0', 'expenvelope >= 0.2.0', 'clockblocks >= 0.2.0',
+    install_requires=['pymusicxml >= 0.1.0', 'expenvelope >= 0.2.0', 'clockblocks >= 0.2.1',
                       'pyfluidsynth', 'sf2utils', 'python-osc'],
     extras_require={
         'lilypond': 'abjad',
