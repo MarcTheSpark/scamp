8ee5e53056bed977c0d2c322461b766b7da58042
MarcTheSpark <marc.p.evans@gmail.com>
2019-06-26 11:25:25 -0700

    Updated test_run to include option of showing notation

--- scamp/test_run.py
+++ scamp/test_run.py
@@ -1,18 +1,28 @@
 from . import Session
 
 
-def play():
+def play(show_lilypond=False, show_xml=False):
     """
     Simple method for determining if scamp was installed correctly. Should play a sequence of pitches telescoping
     towards middle C
     """
-    session = Session()
+    s = Session()
 
-    piano = session.new_part()
+    piano = s.new_part()
 
-    session.set_rate_target(4, 10)
+    if show_xml or show_lilypond:
+        s.start_recording()
+
+    s.set_rate_target(4, 10)
 
     for n in reversed(range(1, 40)):
         piano.play_note(60 + n * (-1) ** n, 1, 0.25)
 
-    piano.play_note(60, 1.0, 6.0)
+    piano.play_note(60, 1.0, 6.25)
+
+    if show_xml or show_lilypond:
+        score = s.stop_recording().to_score()
+        if show_lilypond:
+            score.show()
+        if show_xml:
+            score.show_xml()
